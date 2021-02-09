f1fa6b0b0291cbfc615ea7049efa348dc47c4e03
Ganesh Babu Jothiganesan <readingroomofgb@gmail.com>
2021-02-09 20:16:47 +0530

    modified file

--- function_practice.py
+++ function_practice.py
@@ -3,4 +3,19 @@
 def fun():
     print("Hello world!")
 
-fun()
+#fun()
+
+
+#Function with unknown number of arguments
+
+def fun_with_args(*fruits):
+    print("{var} is one of my favourite fruits".format(var=fruits[1]))
+
+#fun_with_args('mango','apple','banana')
+
+
+#Function with args of key value relationship
+def fun_with_kw(**empdetails):
+    print("{e} is from {s}".format(e=empdetails['name'],s=empdetails['state']))
+
+fun_with_kw(name='john',state='AZ')
