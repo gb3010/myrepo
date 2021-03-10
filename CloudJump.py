cae1cba5d332ac9225843f2239679b7c263c9afb
gb3010 <38236470+gb3010@users.noreply.github.com>
2021-03-10 11:33:58 +0530

    Update CloudJump.py

--- CloudJump.py
+++ CloudJump.py
@@ -1,27 +1,26 @@
 #Jump 2 steps if there is 0 if not jump 1 step
-def Cloudjump(Arr,jump=0):
-    for i in range(len(Arr)-2):
-             if Arr[i+2] == 0 and Arr[i] != 1:
-                     print("Sensing 0 two steps ahead standing at {p}".format(p=i))
-                     i+=2
-                     jump+=1
-                     print("Now at {x} and jump no. {y}".format(x=i,y=jump))
-             else:
-                 #print('no',i)
-                 if Arr[i] == 0:
-                     i+=1
-                     jump+=1
+def jumpingOnClouds(c):
     
-    if i != len(Arr)-1 and Arr[-1] == 0:
-        jump+=1 
+    i=jump=0
+    while i<len(c)-2:
+        if c[i+2] == 0:
+            jump+=1
+            i+=2
+        elif c[i+1] == 0:
+            jump+=1
+            i+=1
+
     
+    if i == len(c)-2 and c[i] == 0 :
+        jump+=1
+
     return jump
     
 
 
 
-k=[0, 1, 0, 0, 1, 0, 0]
-z=[0, 1, 0, 0, 0, 1 ,0]
+#k=[0, 1, 0, 0, 1, 0, 0]
+#z=[0, 1, 0, 0, 0, 1 ,0]
 q=[0, 0, 0, 1, 0, 1, 0]
 k2=Cloudjump(q)
 print(k2)
